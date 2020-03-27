e4e6242734ce8110a36e61de80c3016fa8dff5f4
Maref0705 <60621806+Maref0705@users.noreply.github.com>
2020-03-27 23:17:05 +0700

    Add files via upload

---
+++ Cara-buat-Script.py
@@ -0,0 +1,56 @@
+#Cara-Buat-Script.py
+import os, sys, time
+os.system('clear')
+def main (kata):
+        for e in kata:
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.2)
+print ("""
+\033[1;91m     ____   ____ ____  ___ ____ _____ 
+    / ___| / ___|  _ \|_ _|  _ \_   _|
+    \___ \| |   | |_) || || |_) || |  
+\033[1;37m     ___) | |___|  _ < | ||  __/ | |  
+    |____/ \____|_| \_\___|_|    |_|  
+               \033[1;33mversi \033[1;92m9.9
+""")
+print ("\033[1;91m[\033[1;37m=======================================\033[1;91m]")
+main("""
+        \033[1;37mSelamat datang Gaess \033[1;92m>_
+
+   \033[1;37mSemoga bermanfaat buat kalian \033[1;92m>_<
+
+""")
+print ("\033[1;91m[\033[1;37m=======================================\033[1;91m]")
+nama = input('\033[1;34m( \033[1;33mSTAR \033[1;34m) \033[1;92m--> \033[1;91mEnter \033[1;92m: ')
+time.sleep(3)
+print ('\n')
+main('  \033[1;92mLoading\033[1;37m....')
+time.sleep(4)
+os.system('clear')
+print ("""
+       \033[1;91m(\033[1;33mAuthor\033[1;91m) \033[1;92m: \033[1;37mM_aref
+
+       \033[1;34m(\033[1;37mYoutub\033[1;34m) \033[1;92m: \033[1;33mThe-X-Cyber
+""")
+main("""
+\033[1;37m#Cara-Buat-Script.python
+import os, sys, time
+os.system('clear')
+logo buat sendiri ya gaess :v
+print ("[======================================]")
+print ("[01] : Seterah kalian")
+print ("[02] : Seterah kalian")
+print ("[03] : Seterah kalian")
+print ("[04] : Seterah kalian")
+print ("[05] : Seterah kalian")
+print ("[06] : Seterah kalian")
+print ("[07] : Seterah kalian")
+print ("[08] : Seterah kalian")
+print ("[09] : Seterah kalian")
+print ("[10] : Seterah kalian")
+print ("[00] : Exit")
+print ("[======================================]")
+""")
+time.sleep(4)
+os.system("python3 Cara-buat-Script.py")
